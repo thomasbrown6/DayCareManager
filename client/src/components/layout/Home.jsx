49ae1e57c699be6499bc61e36c0e22b4f7549427
Thomas Brown (Insight Global <v-thobr@microsoft.com>
2020-02-27 11:12:16 -0500

    changed title

--- client/src/components/layout/Home.jsx
+++ client/src/components/layout/Home.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 const Home = ({ isAuthenticated }) => {
-  if(isAuthenticated) {
-    return <Redirect to='/dashboard'/>
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
   }
 
-
   return (
-    <section className='landing'>
-      <div className='dark-overlay'>
-        <div className='landing-inner'>
-          <h1 className='x-large'>Daycare Manager</h1>
-          <p className='lead'>
-            Create a account, keep track of your daycare expenses and amount owed.
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Child Care We Care</h1>
+          <p className="lead">
+            Create a account, keep track of your daycare expenses and amount
+            owed.
           </p>
-          <div className='buttons'>
-            <Link to='/register' className='btn btn-primary'>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
               Sign Up
             </Link>
-            <Link to='/login' className='btn btn-light'>
+            <Link to="/login" className="btn btn-light">
               Login
             </Link>
           </div>
