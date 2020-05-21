f88e93da1528c641b106d0a3c037290304bc15b1
Thomas Brown (Insight Global <v-thobr@microsoft.com>
2020-05-21 11:51:00 -0400

    added build

---
+++ client/build/precache-manifest.ab1cca9fd9c74790ad99e907cfe04b26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d84b3b68b284aabea8725a508fca83f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e4efc243923022debb3",
+    "url": "/static/css/main.59d6d224.chunk.css"
+  },
+  {
+    "revision": "00e6cc324250acbe2bfc",
+    "url": "/static/js/2.da63903f.chunk.js"
+  },
+  {
+    "revision": "0e4efc243923022debb3",
+    "url": "/static/js/main.7a80fba7.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "40503b7ab21d1a38222955f40f4e61ff",
+    "url": "/static/media/daycare_bg.40503b7a.jpg"
+  }
+]);
