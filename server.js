79976483c43b6ecb39eb69f980a5b18672a509cb
Thomas Brown (Insight Global <v-thobr@microsoft.com>
2020-05-21 11:45:08 -0400

    upadte

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/cosmosdb');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./config/cosmosdb");
+const path = require("path");
 
 const app = express();
 
@@ -11,22 +11,22 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 //routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/daycares', require('./routes/api/classrooms'));
-app.use('/api/daycares', require('./routes/api/daycares'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/daycares", require("./routes/api/classrooms"));
+app.use("/api/daycares", require("./routes/api/daycares"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+//if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+//}
 
 const PORT = process.env.PORT || 5000;
 
